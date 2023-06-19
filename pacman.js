e7931f35dc0645801a010c5cf2281ad4f4f5d22f
nathanlao <gla93@sfu.ca>
2023-06-19 16:56:21 -0700

    coe refractor and render green square now

--- pacman.js
+++ pacman.js
@@ -2,10 +2,25 @@
 var canvas;
 var gl;
 
-var programBlueBorders;
+// var programBlueBorders;
 var programGreyCorridors;
-var vBufferBorders;
-var vBufferCorridors;
+var programGreenBlock;
+
+// Four Vertices for grey square
+var verticesGreyCorridors = [
+    vec2( -0.8, -0.8 ),
+    vec2( -0.8, 0.8 ),
+    vec2( 0.8, 0.8 ),
+    vec2( 0.8, -0.8 ),
+];
+
+// Four vertices for a green block
+var verticesGreenBlock = [
+    vec2( -0.6, -0.01 ),
+    vec2( -0.6, 0.6 ),
+    vec2( 0.01, 0.6 ),
+    vec2( 0.01, -0.01 ),
+];
 
 function initializeContext() {
 
@@ -17,110 +32,77 @@ function initializeContext() {
     }
 
     gl.viewport( 0, 0, canvas.width, canvas.height );
-	gl.clearColor( 0.5, 0.5, 0.5, 1.0 ); // grey
+	gl.clearColor( 0.5, 0.5, 0.5, 1.0 ); // grey background color
+
+    // Enable depth test
+    // gl.enable(gl.DEPTH_TEST);
 
     logMessage("WebGL initialized.");
 
 }
 
+
+// Setup
 async function setup() {
     // Initialize the context.
-    initializeContext();
+    initializeContext()
 
-    // Create data for pacman
-    canvasPacman();
+    programGreyCorridors = initShaders( gl, "vertex-shader", "fragment-shader-grey-corridors" )
+    programGreenBlock = initShaders( gl, "vertex-shader", "fragment-shader-green-blocks" )
 
-    // Create vertex buffer data.
-    createBuffers();
-
-    // Load shaders (from html) and initialize attribute buffers
-    programBlueBorders = initShaders( gl, "vertex-shader", "fragment-shader-blue-borders" );
-    programGreyCorridors = initShaders( gl, "vertex-shader", "fragment-shader-grey-corridors" );
-
-    // Create vertex array objects
-    createVertexArrayObjects();
-
-    // Draw!
-    render();
-};
-
-window.onload = setup;
-
-var positionsBlue = [];
-var positionsGrey = [];
-
-function canvasPacman() {
-    // Four Vertices for blue borders
-    var verticesBlueBorders = [
-        vec2( -0.8, -0.8 ),
-        vec2( -0.8, 0.8 ),
-        vec2( 0.8, 0.8 ),
-        vec2( 0.8, -0.8 ),
-    ];
-
-    // Four Vertices for grey square
-    var verticesGreyCorridors = [
-        vec2( -0.8, -0.8 ),
-        vec2( -0.8, 0.8 ),
-        vec2( 0.8, 0.8 ),
-        vec2( 0.8, -0.8 ),
-    ];
-
-    // flatten
-    positionsBlue = flatten(verticesBlueBorders);
-    positionsGrey = flatten(verticesGreyCorridors);
+    render()
 }
 
-function createBuffers() {
+// Create vertex buffer data.
+function createBuffers(vertices) {
     // Load data into GPU
 	// Creating the vertex buffer
-	vBufferBorders = gl.createBuffer();
+    var bufferId = gl.createBuffer()
     // Binding the vertex buffer
-	gl.bindBuffer(gl.ARRAY_BUFFER, vBufferBorders);
-	gl.bufferData( gl.ARRAY_BUFFER,  new Float32Array(positionsBlue), gl.STATIC_DRAW );   
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId )
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW )
 
-    // Load data into GPU for grey square
-    vBufferCorridors = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vBufferCorridors);
-    gl.bufferData( gl.ARRAY_BUFFER,  new Float32Array(positionsGrey), gl.STATIC_DRAW ); 
+    logMessage("Created buffers.")
 
-    logMessage("Created buffers.");
+    return bufferId
 }
 
-function createVertexArrayObjects() { 
+// Create Vertex Array Objects
+function createVertexArrayObjects(bufferId, program) {
     // Associate out shader variables with our data buffer
-    // For blue borders
-    gl.useProgram( programBlueBorders );
-	var vPositionBlue = gl.getAttribLocation( programBlueBorders, "vPosition" );
-    gl.bindBuffer(gl.ARRAY_BUFFER, vBufferBorders);
-	gl.vertexAttribPointer( vPositionBlue, 2, gl.FLOAT, false, 0, 0 );
-	gl.enableVertexAttribArray( vPositionBlue );
-    
-    // For grey square
-    gl.useProgram( programGreyCorridors );
-    var vPositionGrey = gl.getAttribLocation( programGreyCorridors, "vPosition" );
-    gl.bindBuffer(gl.ARRAY_BUFFER, vBufferCorridors);
-    gl.vertexAttribPointer( vPositionGrey, 2, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPositionGrey );
-    
-    logMessage("Created VAOs.");
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId )
+    gl.useProgram( program )
+    var vPosition = gl.getAttribLocation( program, "vPosition" )
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 )
+    gl.enableVertexAttribArray( vPosition )
+
+    logMessage("Created VAOs.")
 }
 
+// Render TRIANGLE_FAN
+function renderTriangleFan(vertices, program) {
+    gl.useProgram( program )
+    gl.drawArrays( gl.TRIANGLE_FAN, 0, vertices.length )
+}
 
+// Render
 function render() {
-	// Clearing the buffer
-	gl.clear( gl.COLOR_BUFFER_BIT ); 
+    // Clearing the buffer
+	gl.clear( gl.COLOR_BUFFER_BIT)
 
-    // Blue borders
-    gl.useProgram( programBlueBorders );
-    gl.bindBuffer(gl.ARRAY_BUFFER, vBufferBorders);
-	gl.drawArrays( gl.LINE_LOOP, 0, 4 ); // line loop for border
+    // Grey Corridors
+    var bufferIdGrey = createBuffers(verticesGreyCorridors)
+    createVertexArrayObjects(bufferIdGrey, programGreyCorridors)
+    renderTriangleFan(verticesGreyCorridors, programGreyCorridors)
 
-    // Draw grey square
-    gl.useProgram( programGreyCorridors );
-    gl.bindBuffer(gl.ARRAY_BUFFER, vBufferCorridors);
-    gl.drawArrays( gl.TRIANGLE_FAN, 0, 4 );
+    // Green Obstacles
+    var bufferIdGreen = createBuffers(verticesGreenBlock)
+    createVertexArrayObjects(bufferIdGreen, programGreenBlock)
+    renderTriangleFan(verticesGreenBlock, programGreenBlock)
 }
+
+window.onload = setup
+
 
 // Logging
 function logMessage(message) {
